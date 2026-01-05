7c6765471aa4c3399e7175519844d4a105da4446
mayurmathe <mayurmathe007@gmail.com>
2026-01-05 14:15:04 +0530

    changes done

--- src/main/java/AtumationProject/gitpull.java
+++ src/main/java/AtumationProject/gitpull.java
@@ -3,7 +3,7 @@ package AtumationProject;
 public class gitpull {
 	public static void main(String[] args) {
 		System.out.println("welcome");
-		System.out.println("hii");
+		System.out.println("hello");
 	}
 
 
